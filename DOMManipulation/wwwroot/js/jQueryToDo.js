4254c26a7fbd062e11f70def7715a8023550701a
Duy Thai <duy.thai@altsrc.net>
2019-05-15 17:57:02 +0700

    jQuery ToDo list

---
+++ DOMManipulation/wwwroot/js/jQueryToDo.js
@@ -0,0 +1,17 @@
+(function ($) {
+    // Remove
+    var listRoot = $("#main_list");
+    listRoot.on("click", function (event) {
+        var button = $(event.target).closest("BUTTON");
+        if (button.length === 0)
+            return;
+        button.closest("LI").remove();
+    });
+    // Add
+    var mainAdd = $("#main_add");
+    mainAdd.click(function (event) {
+        var toAdd = "<li class=\"list-group-item\">\n                <button type=\"button\" class=\"btn btn-sm\" title=\"remove\">\n                    <span class=\"glyphicon glyphicon-minus\" aria-hidden=\"true\"></span>\n                </button>\n                <span>" + $("#main_input").val() + "</span>\n            </li>";
+        listRoot.append(toAdd);
+    });
+})(jQuery);
+//# sourceMappingURL=jQueryToDo.js.map
